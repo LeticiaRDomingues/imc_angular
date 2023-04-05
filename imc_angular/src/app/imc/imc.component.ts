1eb6f5f11cda8efd5c0295cbf8bb10744448f941
leticiarosadominguescontato@gmail.com <leticiardominguescontato@gmail.com>
2023-04-04 23:30:30 -0300

    Versao 4

--- imc_angular/src/app/imc/imc.component.ts
+++ imc_angular/src/app/imc/imc.component.ts
@@ -5,15 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './imc.component.html',
   styleUrls: ['./imc.component.css']
 })
+
 export class ImcComponent {
-  //Atributos de classe/componente
-  peso : number = 0;
+
   altura : number = 0;
+  peso : number = 0;
   resp : number = 0;
 
+  resultado : string = '';
+
   result(){
-    this.resp = this.peso / (this.altura*this.altura);
+    this.resp = this.peso / ((this.altura/100)*(this.altura/100));
+    this.resp = parseFloat(this.resp.toFixed(2));
 
+
+    if(this.resp < 18.5){
+      this.resultado = 'Magreza!!';
+    }
+    else if(this.resp < 25){
+    this.resultado = 'Normal!!';
+    }
+    else if(this.resp < 30){
+    this.resultado = 'sobrepeso!!';
+    }
+    else if(this.resp < 40){
+    this.resultado = 'Obesidade II!!';
+    }
+    else if(this.resp > 40){
+    this.resultado = 'Obesidade III!!';
+    }
   }
-
 }
+
